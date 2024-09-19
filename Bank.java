550a5cb522a39ee0ae196869c0381466cc2ef1e4
Shaurya Singh <72272831+shauryasingh0302@users.noreply.github.com>
2024-09-20 03:17:37 +0530

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,113 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Bank {
+    private static int id = 1;
+    private String name;
+    private String password;
+    private int accountId;
+    private int balance;
+    private static ArrayList<Bank> accounts = new ArrayList<>();
+
+    public Bank(String name, String password) {
+        this.name = name;
+        this.password = password;
+        this.accountId = id++;
+        this.balance = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public boolean authenticate(String pass) {
+        return this.password.equals(pass);
+    }
+
+    public void withdraw(int amount) {
+        this.balance -= amount;
+    }
+
+    public void deposit(int amount) {
+        this.balance += amount;
+    }
+
+    public static void createAccount(String name, String password) {
+        Bank newAccount = new Bank(name, password);
+        accounts.add(newAccount);
+        System.out.println("Hello "+newAccount.getName()+"! Your account is successfully created!!\nYour Account ID is "+newAccount.getAccountId());
+    }
+
+    public static Bank getAccount(int id) {
+        for (Bank account : accounts) {
+            if (account.getAccountId() == id) {
+                return account;
+            }
+        }
+        return null;
+    }
+
+    public static void update(Bank account, String newName) {
+        account.name = newName;
+    }
+
+    public static void delete(int delId) {
+        Bank accDel = getAccount(delId);
+        if (accDel != null) {
+            accounts.remove(accDel);
+            System.out.println("Dear "+accDel.getName()+" your account has been successfully deleted!!");
+        }
+    }
+
+    public static void menu(Bank account) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1. Transfer Money\n2. Deposit\n3. Withdraw\n4. Show Current Balance");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                scanner.nextLine();
+                System.out.println("Enter Amount to transfer:");
+                int amount = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("Transfer to whom (Account Id)");
+                int payeeId = scanner.nextInt();
+                Bank payee = Bank.getAccount(payeeId);
+                if (account.getBalance() > amount) {
+                    account.withdraw(amount);
+                    payee.deposit(amount);
+                } else {
+                    System.out.println("Insufficient Balance!!");
+                }
+                System.out.println("Transferred Successfully!!");
+                System.out.println("Successful! Your current balance is Rs." + account.getBalance());
+                break;
+            case 2:
+                System.out.println("Enter amount to deposit:");
+                int depositAmount = scanner.nextInt();
+                scanner.nextLine();
+                account.deposit(depositAmount);
+                System.out.println("Successful! Your current balance is Rs." + account.getBalance());
+                break;
+            case 3:
+                System.out.println("Enter amount to withdraw:");
+                int withdrawAmount = scanner.nextInt();
+                scanner.nextLine();
+                account.withdraw(withdrawAmount);
+                System.out.println("Successful! Your current balance is Rs." + account.getBalance());
+                break;
+            case 4:
+                System.out.println("Your current balance is Rs." + account.getBalance());
+                break;
+            default:
+                System.out.println("Invalid Choice!!");
+        }
+    }
+}
---
+++ main.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("Select an option to start:\n1-Create Account\n2-Use Account\n3-Update Account\n4-Delete Account");
+            int inputChoice = scanner.nextInt();
+            switch (inputChoice) {
+                case 1:
+                    scanner.nextLine();
+                    System.out.println("Enter your name: ");
+                    String name = scanner.nextLine();
+                    System.out.println("Create a password: ");
+                    String password = scanner.nextLine();
+                    Bank.createAccount(name, password);
+                    break;
+
+                case 2:
+                    System.out.println("Enter Account ID");
+                    int x = scanner.nextInt();
+                    scanner.nextLine();
+                    Bank account2 = Bank.getAccount(x);
+                    System.out.println("Hello " + account2.getName() + " please enter Password to proceed:");
+                    String pass2 = scanner.nextLine();
+                    if (account2.authenticate(pass2)) {
+                        Bank.menu(account2);
+                    } else {
+                        System.out.println("Incorrect Account Id or Password\n Please try again!!");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Enter Account ID to update     details:");
+                    int y = scanner.nextInt();
+                    scanner.nextLine();
+                    Bank account3 = Bank.getAccount(y);
+                    System.out.println("Hello " + account3.getName() + " please enter Password to proceed:");
+                    String pass3 = scanner.nextLine();
+                    if (account3.authenticate(pass3)) {
+                        System.out.println("Enter New Name:\nLeave Empty to skip");
+                        String a = scanner.nextLine();
+                        if (!a.equals("")) {Bank.update(account3, a);
+                        System.out.println("Name updated Successfully!!");
+                        }
+                    } else {
+                        System.out.println("Incorrect Account Id or Password\n Please try again!!");
+                    }
+                    break;
+                case 4:
+                    System.out.println("Enter Account ID to be deleted:");
+                    int input4 = scanner.nextInt();
+                    scanner.nextLine();
+                    Bank account4 = Bank.getAccount(input4);
+                    System.out.println("Hello " + account4.getName() + " please enter Password to proceed:");
+                    String pass4 = scanner.nextLine();
+                    if (account4.authenticate(pass4)) {
+                    Bank.delete(input4);
+                    } else {
+                        System.out.println("Incorrect Account Id or Password\n Please try again!!");
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid Choice!!");
+
+            }
+        }
+
+    }
+}

a282deb5009ab70840ed63619fdac5db31b3c262
Shaurya Singh <72272831+shauryasingh0302@users.noreply.github.com>
2024-09-20 03:20:28 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class Main {
+class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while(true){
